0f0302e94d6e2f3cb978f79b62866ac2508c24a4
Clara <10088152+claraj@users.noreply.github.com>
2021-11-12 13:24:45 -0600

    Create spells.cs

---
+++ spells.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Spells
+{
+    class Spells
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example repo");
+        }
+    }
+}
